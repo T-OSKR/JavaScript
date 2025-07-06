9eae1e024d253b4db6b9e0924e69f3c80fe5445f
Ratishwer <ratishwersingh@gmail.com>
2025-07-06 15:36:00 +0530

    final commit

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,11 @@
 let score = "hitesh"
 
-//console.log(typeof score);
-//console.log(typeof(score));
+// console.log(typeof score); --> string
+// console.log(typeof(score)); -- string
 
 let valueInNumber = Number(score)
-//console.log(typeof valueInNumber);
-//console.log(valueInNumber);
+// console.log(typeof valueInNumber); --> number
+// console.log(valueInNumber); --> NaN (eg: '33bac'), if score: null=0, undefined=NaN, bool(true/false)=1/0
 
 
 // "33" => 33
@@ -15,7 +15,7 @@ let valueInNumber = Number(score)
 let isLoggedIn = "hitesh"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-//  console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn); 
 
 // 1 => true; 0 => false
 // "" => false
@@ -24,14 +24,14 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-// console.log(stringNumber);
-// console.log(typeof stringNumber);
+// console.log(stringNumber); --> 33
+// console.log(typeof stringNumber); --> string
 
 // *********************** Operations ***********************
 
 let value = 3
 let negValue = -value
-// console.log(negValue);
+// console.log(negValue); --> -3
 
 // console.log(2+2);
 // console.log(2-2);
@@ -41,15 +41,19 @@ let negValue = -value
 // console.log(2%3);
 
 let str1 = "hello"
-let str2 = " hitesh"
+let str2 = "hitesh"
 
 let str3 = str1 + str2
-// console.log(str3);
+// console.log(str3); --> hellohitesh
 
-// console.log("1" + 2);
-// console.log(1 + "2");
-// console.log("1" + 2 + 2);
-// console.log(1 + 2 + "2");
+// console.log("1" + 2); -->12
+// console.log(1 + "2"); --> 12
+// console.log("1" + 2 + 2); --> 122
+// console.log(1 + 2 + "2"); --> 32
+           
+// console.log(1 + 22 + "33" +1 + 10); --> 2333110
+
+// In upper practices if the string is first whole will be treated as string and if the string is at last then first perform operaions and the convert...
 
 // console.log( (3 + 4) * 5 % 3);
 
@@ -66,4 +70,3 @@ console.log(gameCounter);
 
 // link to study
 // https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
-// this is a sample comment Selection
